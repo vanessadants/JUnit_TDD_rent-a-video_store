6811368854cd8ce53fb8dc5602995a7dd6e125fa
vanessadants <vanessa.dantas796@gmail.com>
2019-03-24 02:22:26 -0300

    implementation of TDD tests on a rent-a-store example: first steps

---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,76 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
+		if(usuario == null) {
+			throw new LocadoraException("Usuario vazio");
+		}
+		
+		if(filmes == null || filmes.isEmpty() || filmes.stream().anyMatch(Objects::isNull)) {
+			throw new LocadoraException("Existe filme vazio");
+		}
+		
+		if(filmes.size() > 6) {
+			throw new LocadoraException("O máximo de filmes permitidos para aluguel é 6.");
+		}
+		
+		if(filmes.stream().anyMatch(f -> f.getEstoque() == 0)) {
+			throw new FilmeSemEstoqueException();
+		}
+		
+		Locacao locacao = new Locacao();
+		locacao.setFilmes(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(getValorTotalAluguel(filmes));
+
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		return locacao;
+	}
+
+	private double getValorTotalAluguel(List<Filme> filmes) {
+		double total=0;
+		for (int i = 0; i < filmes.size(); i++) {
+			total += filmes.get(i).getPrecoLocacao()*getPercentualPorFilme(i+1);
+		}
+		return total;
+	}
+
+	private Double getPercentualPorFilme(int numeroFilme) {
+		double percentual = 1;
+		switch (numeroFilme) {
+			case 3:
+				percentual = 0.75;
+				break;
+			case 4:
+				percentual = 0.5;
+				break;
+			case 5:
+				percentual = 0.25;
+				break;
+			case 6:
+				percentual = 0;
+				break;
+			default:
+				break;
+			}
+		return percentual;
+	}
+	
+}
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,135 @@
+package br.ce.wcaquino.servicos;
+
+
+
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+
+public class LocacaoServiceTest {
+
+	private LocacaoService service;
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Before
+	public void setup(){
+		service = new LocacaoService();
+	}
+	
+	@Test
+	public void testeLocacao() throws Exception {
+		//cenario
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme1 = new Filme("Filme 1", 1, 5.0);
+		Filme filme2 = new Filme("Filme 2", 2, 2.5);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, Arrays.asList(filme1, filme2));
+			
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(7.5)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+	}
+	
+	@Test(expected = FilmeSemEstoqueException.class)
+	public void testLocacao_filmeSemEstoque() throws Exception{
+		//cenario
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme1 = new Filme("Filme 1", 1, 4.0);
+		Filme filme2 = new Filme("Filme 2", 0, 6.0);
+		
+		//acao
+		service.alugarFilme(usuario, Arrays.asList(filme1, filme2));
+	}
+	
+	@Test
+	public void testLocacao_usuarioVazio() throws FilmeSemEstoqueException{
+		//cenario
+		Filme filme1 = new Filme("Filme 1", 1, 4.0);
+		Filme filme2 = new Filme("Filme 2", 1, 4.5);
+		
+		//acao
+		try {
+			service.alugarFilme(null, Arrays.asList(filme1,filme2));
+			Assert.fail();
+		} catch (LocadoraException e) {
+			assertThat(e.getMessage(), is("Usuario vazio"));
+		}
+	}
+
+	@Test
+	public void testLocacao_FilmeVazio() throws FilmeSemEstoqueException, LocadoraException{
+		//cenario
+		Usuario usuario = new Usuario("Usuario 1");
+		
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("Existe filme vazio");
+		
+		//acao
+		service.alugarFilme(usuario, null);
+	}
+	
+	@Test
+	public void testeLocacaoComDescontoCrescente() throws Exception {
+		//cenario
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme1 = new Filme("Godzila", 1, 25d);
+		Filme filme2 = new Filme("Pinoquio", 1, 20d);
+		Filme filme3 = new Filme("O menino", 1, 30d);
+		Filme filme4 = new Filme("Forrest", 1, 22d);
+		Filme filme5 = new Filme("Ariel", 1, 28d);
+		Filme filme6 = new Filme("High School", 1, 24d);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6));
+			
+		//verificacao
+		error.checkThat(locacao.getValor(), is(equalTo(85.5)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+	}
+	
+	@Test
+	public void testeLocacaoMaiorQueLimite() throws Exception {
+		//cenario
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("O máximo de filmes permitidos para aluguel é 6.");
+		
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme1 = new Filme("Godzila", 1, 25d);
+		Filme filme2 = new Filme("Pinoquio", 1, 20d);
+		Filme filme3 = new Filme("O menino", 1, 30d);
+		Filme filme4 = new Filme("Forrest", 1, 22d);
+		Filme filme5 = new Filme("Ariel", 1, 28d);
+		Filme filme6 = new Filme("High School", 1, 24d);
+		Filme filme7 = new Filme("High School 2", 1, 24d);
+		
+		//acao
+		service.alugarFilme(usuario, Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7));
+	}
+	
+}
